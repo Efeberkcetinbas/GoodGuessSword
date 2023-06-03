a98d1a83b8920f49e5626e3809a585f0666c12ec
Efe Berk Çetinbaş <berkefefast@gmail.com>
2023-06-03 18:22:14 +0300

    - Panel Manager Improved. - Scene UI objects connection added

--- Assets/Scripts/Managers/PanelManager.cs
+++ Assets/Scripts/Managers/PanelManager.cs
@@ -6,18 +6,23 @@ using DG.Tweening;
 
 public class PanelManager : MonoBehaviour
 {
-    [SerializeField] private RectTransform StartPanel,CharacterPanel,WeaponPanel;
+    [SerializeField] private RectTransform StartPanel,CharacterPanel,MapPanel,SettingsPanel;
 
 
     [SerializeField] private Image Fade;
 
-    [SerializeField] private float StartX,StartY,CharacterX,CharacterY,WeaponX,WeaponY,duration;
+    [SerializeField] private float StartX,StartY,CharacterX,CharacterY,MapX,MapY,duration;
+
+    [SerializeField] private List<GameObject> characters=new List<GameObject>();
+
+    [SerializeField] private GameObject backButton,settingsButton;
+
 
     
 
     private bool oneTime=true;
 
-    private void OnEnable() 
+    /*private void OnEnable() 
     {
         EventManager.AddHandler(GameEvent.OnNextLevel,OnNextLevel);
     }
@@ -26,9 +31,9 @@ public class PanelManager : MonoBehaviour
     private void OnDisable() 
     {
         EventManager.RemoveHandler(GameEvent.OnNextLevel,OnNextLevel);
-    }
+    }*/
     
-    private void Update() 
+    /*private void Update() 
     {
         if(oneTime)
         {
@@ -38,7 +43,7 @@ public class PanelManager : MonoBehaviour
                 oneTime=false;
             }
         }
-    }
+    }*/
 
     private void OnNextLevel()
     {
@@ -62,23 +67,41 @@ public class PanelManager : MonoBehaviour
 
     }
 
+    public void OpenSettingsMenu()
+    {
+        SettingsPanel.gameObject.SetActive(true);
+        settingsButton.SetActive(false);
+        SettingsPanel.transform.DOScale(Vector3.one,0.3f).SetEase(Ease.OutBounce);
+    }
+
+    public void CloseSettingsMenu()
+    {
+        SettingsPanel.transform.DOScale(Vector3.zero,0.1f).OnComplete(()=>{
+            SettingsPanel.gameObject.SetActive(false);
+            settingsButton.SetActive(true);
+        });
+    }
+
 
     public void OpenCharacterPanel()
     {
         oneTime=false;
+        backButton.SetActive(false);
         //EventManager.Broadcast(GameEvent.OnButtonClicked);
         StartPanel.DOAnchorPos(new Vector2(StartX,StartY),duration).OnComplete(()=>StartPanel.gameObject.SetActive(false));
         CharacterPanel.gameObject.SetActive(true);
-        CharacterPanel.DOAnchorPos(Vector2.zero,duration);
+        CharacterPanel.DOAnchorPos(Vector2.zero,duration).OnComplete(()=>StartCoroutine(CharactersAnimations(true)));
+        
     }
 
-    public void OpenWeaponPanel()
+    public void OpenMapPanel()
     {
         oneTime=false;
+        //OnOpenMap Eventi Firlat bu sayede kamera map'in oldugu yere handler ile gider
         //EventManager.Broadcast(GameEvent.OnButtonClicked);
         StartPanel.DOAnchorPos(new Vector2(StartX,StartY),duration).OnComplete(()=>StartPanel.gameObject.SetActive(false));
-        WeaponPanel.gameObject.SetActive(true);
-        WeaponPanel.DOAnchorPos(Vector2.zero,duration);
+        MapPanel.gameObject.SetActive(true);
+        MapPanel.DOAnchorPos(Vector2.zero,duration);
     }
 
     public void BackToStart(bool isOnCharacter)
@@ -87,17 +110,37 @@ public class PanelManager : MonoBehaviour
         if(isOnCharacter)
         {
             StartPanel.gameObject.SetActive(true);
+            StartCoroutine(CharactersAnimations(false));
             StartPanel.DOAnchorPos(Vector2.zero,duration).OnComplete(()=>oneTime=true);
-            CharacterPanel.DOAnchorPos(new Vector2(CharacterX,CharacterY),duration).OnComplete(()=>CharacterPanel.gameObject.SetActive(false));
+            CharacterPanel.DOAnchorPos(new Vector2(CharacterX,CharacterY),duration).OnComplete(()=>{
+                CharacterPanel.gameObject.SetActive(false);
+            });
         }
         else
         {
             StartPanel.gameObject.SetActive(true);
             StartPanel.DOAnchorPos(Vector2.zero,duration).OnComplete(()=>oneTime=true);
-            WeaponPanel.DOAnchorPos(new Vector2(WeaponX,WeaponY),duration).OnComplete(()=>WeaponPanel.gameObject.SetActive(false));
+            MapPanel.DOAnchorPos(new Vector2(MapX,MapY),duration).OnComplete(()=>MapPanel.gameObject.SetActive(false));
         }
 
         //EventManager.Broadcast(GameEvent.OnButtonClicked);
+    }
 
+    private IEnumerator CharactersAnimations(bool openCharacter)
+    {
+        for (int i = 0; i < characters.Count; i++)
+        {
+            characters[i].transform.localScale=Vector3.zero;
+        }
+
+        if(openCharacter)
+        {
+            for (int i = 0; i < characters.Count; i++)
+            {
+                characters[i].transform.DOScale(1,0.1f).SetEase(Ease.OutBounce);
+                yield return new WaitForSeconds(0.25f);
+            }
+            backButton.SetActive(true);
+        }
     }
 }
